272205b71ea9bdae72448bd0c237bed6fd7cc4b0
Zarina Raimkulova <zarinar1105@gmail.com>
2021-03-23 19:36:13 +0100

    Added dice class and showing of rolled number

--- app/src/main/java/raim/zarin/diceroller/MainActivity.kt
+++ app/src/main/java/raim/zarin/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package raim.zarin.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +11,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
+
         rollButton.setOnClickListener {
-            Toast.makeText(this,  "Dice Rolled!", Toast.LENGTH_SHORT).show()
+            rollDice()
+            // Toast.makeText(this,  "Dice Rolled!", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(120)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+
+}
+
+class Dice(private val numSides: Int = 6) {
+
+    fun roll(): Int{
+        return (1..numSides).random()
     }
 }
