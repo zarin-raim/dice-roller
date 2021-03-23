03f2054850d655d2b7195ef12a41198035c0d6e6
Zarina Raimkulova <zarinar1105@gmail.com>
2021-03-23 19:17:55 +0100

    Initial commit

---
+++ app/src/main/java/raim/zarin/diceroller/MainActivity.kt
@@ -0,0 +1,18 @@
+package raim.zarin.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            Toast.makeText(this,  "Dice Rolled!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
